537b6fb864349ec0b1d9fb5caa12bad0979e4827
Halim <Halimml751@gmail.com>
2025-01-04 14:05:40 +0700

    update

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ SECRET_KEY = '12g3h1b2n3b12jhv3h1b2kh3b12bkKBKHBKJHb'
 class Config:
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql://{DB_USER}:{DB_PASSWORD}"
-        f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        f"@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = SECRET_KEY
