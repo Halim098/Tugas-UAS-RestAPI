80bc7264d48679295ddaeb8e4852f1eec1790d18
Halim <Halimml751@gmail.com>
2025-01-04 17:34:34 +0700

    updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from app.models import db
 from flask_jwt_extended import JWTManager
 from app.routes import api
@@ -8,8 +9,11 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)  # Gunakan konfigurasi dari config.py
 
+    # Aktifkan CORS dengan pengaturan sesuai dengan yang Anda inginkan
+    CORS(app, resources={r"/*": {"origins": "*", "methods": ["GET", "POST", "PUT", "DELETE"], "allow_headers": ["Content-Type", "Authorization"], "expose_headers": ["Content-Length"]}})
+
     db.init_app(app)
     JWTManager(app)
     app.register_blueprint(api, url_prefix='/api')
-    
+
     return app
