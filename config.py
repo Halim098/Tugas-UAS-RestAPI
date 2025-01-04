7e197c0aea7b31ff7f9e0023250b46e744cd38a9
Halim <Halimml751@gmail.com>
2025-01-04 14:10:00 +0700

    update

--- config.py
+++ config.py
@@ -1,16 +1,4 @@
-DB_HOST = 'db.mlvjbuusruadndqlhyop.supabase.co'
-DB_USER = 'postgres'
-DB_PASSWORD = 'mXK8f5R32A3awd4W'
-DB_NAME = 'postgres'
-DB_PORT = '5432'
-
-SECRET_KEY = '12g3h1b2n3b12jhv3h1b2kh3b12bkKBKHBKJHb'
-
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{DB_USER}:{DB_PASSWORD}"
-        f"@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
-    )
+    SQLALCHEMY_DATABASE_URI = '"postgresql://postgres:mXK8f5R32A3awd4W@db.mlvjbuusruadndqlhyop.supabase.co:5432/postgres?sslmode=require"'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = SECRET_KEY
+    SECRET_KEY = '12g3h1b2n3b12jhv3h1b2kh3b12bkKBKHBKJHb'
